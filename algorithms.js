54c0e0c422efee6da3797620de10e112792aa3eb
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2019-03-05 21:30:31 -0800

    another example

--- algorithms.js
+++ algorithms.js
@@ -13,17 +13,41 @@ const nemo = ["nemo"]
 
 console.log(findnemo(nemo)) //O(n) --> linear time, n depends on the number of input;
 
-
+/////////////////////////////////
+// EXAMPLE 2
 function funChallenge(input) {
-    let a = 10
-    a = 50 + 3
+    let a = 10 // 0(1)
+    a = 50 + 3 // 0(1)
 
-    for(let i = 0; i < input.length; i++){
-        anotherFunction();
-        let stranger = true;
-        a++
+    for(let i = 0; i < input.length; i++){ //0(n) n=input Loops are linear Time
+        anotherFunction();    //O(n)
+        let stranger = true;  //0(n)
+        a++ // 0(n)
     }
-    return a;
+    return a; // 0(1)
 }
 
 funChallenge()
+
+// BIG O (3 + 4n)
+
+/////////////////////////////////
+//EXAMPLE 3
+function anotherFunChallenge(input) {
+    let a = 5;
+    let b = 10;
+    let c = 50;
+    for (let i = 0; i < input; i++) {
+      let x = i + 1;
+      let y = i + 2;
+      let z = i + 3;
+  
+    }
+    for (let j = 0; j < input; j++) {
+      let p = j * 2;
+      let q = j * 2;
+    }
+    let whoAmI = "I don't know";
+  }
+
+  // BIG O (4 + 5n)
